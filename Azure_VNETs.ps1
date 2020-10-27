806f515aeba68916e54e2431c3a20496b7d657e8
Jinesh Shah <jinsha@microsoft.com>
2020-10-27 15:58:05 +0530

    initial commit

---
+++ Azure_VNETs.ps1
@@ -0,0 +1,11 @@
+$subs = Get-AzureRmSubscription
+foreach ($Sub in $Subs) {
+	$SelectSub = Select-AzureRmSubscription -SubscriptionName $Sub.Name
+	$VNETs = Get-AzureRmVirtualNetwork
+	foreach ($VNET in $VNETs) {
+		$Sub.Name
+		$VNET.Name
+		($VNET).AddressSpace.AddressPrefixes
+		Write-Host " "
+	}
+}
